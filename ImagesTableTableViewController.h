6cf2a8c1ca92894714df68f0de2bf99781061571
Diego Aguirre <diego3884@gmail.com>
2015-05-04 12:36:27 -0400

    Checkpoint 26 Building Blocstagram complete

---
+++ ImagesTableTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImagesTableTableViewController.h
+//  Blocstagram_V3
+//
+//  Created by Diego Aguirre on 5/4/15.
+//  Copyright (c) 2015 Diego Aguirre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableTableViewController : UITableViewController
+
+@property (nonatomic,strong) NSMutableArray *images;
+
+@end
