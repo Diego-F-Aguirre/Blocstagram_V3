fa390f1a819cd8172d32c8236adf588886e42951
Diego Aguirre <diego3884@gmail.com>
2015-05-04 11:02:00 -0400

    initial commit

---
+++ Blocstagram_V3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram_V3
+//
+//  Created by Diego Aguirre on 5/4/15.
+//  Copyright (c) 2015 Diego Aguirre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
