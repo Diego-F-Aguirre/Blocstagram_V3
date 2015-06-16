7b43e605923c2acac39e633bc1a26520d4c27f24
Diego Aguirre <diego3884@gmail.com>
2015-06-16 08:11:30 -0400

    Added Comment View

---
+++ ComposeCommentView.h
@@ -0,0 +1,30 @@
+//
+//  ComposeCommentView.h
+//  Blocstagram_V3
+//
+//  Created by Diego Aguirre on 6/10/15.
+//  Copyright (c) 2015 Diego Aguirre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ComposeCommentView;
+
+@protocol ComposeCommentViewDelegate <NSObject>
+
+- (void) commentViewDidPressCommentButton:(ComposeCommentView *)sender;
+- (void) commentView:(ComposeCommentView *)sender textDidChange:(NSString *)text;
+- (void) commentViewWillStartEditing:(ComposeCommentView *)sender;
+
+@end
+
+@interface ComposeCommentView : UIView
+
+@property (nonatomic, weak) NSObject <ComposeCommentViewDelegate> *delegate;
+@property (nonatomic, assign) BOOL isWritingComment;
+@property (nonatomic, strong) NSString *text;
+
+- (void) stopComposingComment;
+//- (void) bounceComment;
+
+@end
--- MediaTableViewCell.m
+++ MediaTableViewCell.m
@@ -11,8 +11,9 @@
 #import "Comment.h"
 #import "User.h"
 #import "LikeButton.h"
+#import "ComposeCommentView.h"
 
-@interface MediaTableViewCell() <UIGestureRecognizerDelegate>
+@interface MediaTableViewCell() <UIGestureRecognizerDelegate, ComposeCommentViewDelegate>
 
 @property (nonatomic, strong) UIImageView *mediaImageView;
 @property (nonatomic, strong) UILabel *usernameAndCaptionLabel;
@@ -28,6 +29,7 @@
 
 @property (nonatomic, strong) LikeButton *likeButton;
 @property (nonatomic, weak) UILabel *likeCounter;
+@property (nonatomic,strong) ComposeCommentView *commentView;
 
 @end
 
@@ -103,28 +105,33 @@ static UIColor *commentLabelTextColor;
         [self.likeButton addTarget:self action:@selector(likePressed:) forControlEvents:UIControlEventTouchUpInside];
         self.likeButton.backgroundColor = usernameLabelGray;
         
-        self.likeCounter = [[UILabel alloc]initWithFrame:CGRectMake(200, 100, 20, 20)];
-        self.likeCounter.numberOfLines = 0;
-        self.likeCounter.backgroundColor = commentLabelGray;
-        self.likeCounter.textColor = commentLabelTextColor;
+        self.commentView = [[ComposeCommentView alloc]init];
+        self.commentView.delegate = self;
+        
+        
+//        self.likeCounter = [[UILabel alloc]initWithFrame:CGRectMake(200, 100, 20, 20)];
+//        self.likeCounter.numberOfLines = 0;
+//        self.likeCounter.backgroundColor = commentLabelGray;
+//        self.likeCounter.textColor = commentLabelTextColor;
         
         
     
     
         
-        for (UIView *view in @[self.mediaImageView,self.usernameAndCaptionLabel,self.commentLabel, self.likeButton]) {
+        for (UIView *view in @[self.mediaImageView,self.usernameAndCaptionLabel,self.commentLabel, self.likeButton, self.commentView]) {
         
             [self.contentView addSubview:view];
             view.translatesAutoresizingMaskIntoConstraints = NO;
         }
         
-        NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(_mediaImageView, _usernameAndCaptionLabel, _commentLabel, _likeButton);
+        NSDictionary *viewDictionary = NSDictionaryOfVariableBindings(_mediaImageView, _usernameAndCaptionLabel, _commentLabel, _likeButton, _commentView);
         
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_mediaImageView]|" options:kNilOptions metrics:nil views:viewDictionary]];
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_usernameAndCaptionLabel][_likeButton(==38)]|" options:NSLayoutFormatAlignAllTop | NSLayoutFormatAlignAllBottom metrics:nil views:viewDictionary]];
         [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_commentLabel]|" options:kNilOptions metrics:nil views:viewDictionary]];
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[_commentView]|" options:kNilOptions metrics:nil views:viewDictionary]];
         
-        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_mediaImageView][_usernameAndCaptionLabel][_commentLabel]" options:kNilOptions metrics:nil views:viewDictionary]];
+        [self.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[_mediaImageView][_usernameAndCaptionLabel][_commentLabel][_commentView(==100)]" options:kNilOptions metrics:nil views:viewDictionary]];
         
         
         
@@ -235,6 +242,7 @@ static UIColor *commentLabelTextColor;
         self.usernameAndCaptionLabel.attributedText = [self usernameAndCaptionString];
         self.commentLabel.attributedText = [self commentString];
     self.likeButton.likeButtonState = mediaItem.likeState;
+    self.commentView.text = mediaItem.temporaryComment;
     
     
     }
@@ -253,7 +261,7 @@ static UIColor *commentLabelTextColor;
     
     
          // Get the actual height required for the cell
-        return CGRectGetMaxY(layoutCell.commentLabel.frame);
+        return CGRectGetMaxY(layoutCell.commentView.frame);
 }
 
 - (UILabel *) Counter {
@@ -303,6 +311,27 @@ static UIColor *commentLabelTextColor;
     return self.isEditing == NO;
 }
 
+#pragma mark - ComposeCommentViewDelegate
+
+- (void) commentViewDidPressCommentButton:(ComposeCommentView *)sender{
+
+    [self.delegate cell:self didComposeComment:self.mediaItem.temporaryComment];
+}
+
+- (void) commentView:(ComposeCommentView *)sender textDidChange:(NSString *)text{
+
+    self.mediaItem.temporaryComment = text;
+}
+
+- (void) commentViewWillStartEditing:(ComposeCommentView *)sender{
+
+    [self.delegate cellWillStartComposingComment:self];
+}
+
+- (void) stopComposingComment{
+
+    [self.commentView stopComposingComment];
+}
 
 
 
