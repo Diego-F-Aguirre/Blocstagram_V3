39c0bb3ee51f526523a286397cb19ab7dfefbc05
Diego Aguirre <diego3884@gmail.com>
2015-07-01 20:44:24 -0400

    Photo filters and sending to Instagram

---
+++ PostToInstagramViewController.h
@@ -0,0 +1,15 @@
+//
+//  PostToInstagramViewController.h
+//  Blocstagram_V3
+//
+//  Created by Diego Aguirre on 6/30/15.
+//  Copyright © 2015 Diego Aguirre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostToInstagramViewController : UIViewController
+
+- (instancetype) initWithImage:(UIImage *)sourceImage;
+
+@end
